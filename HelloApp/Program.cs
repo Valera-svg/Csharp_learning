a1ffb2a95fb0c574d5741a44ee1ea33000262cca
Valera-svg <215827524+Valera-svg@users.noreply.github.com>
2025-11-20 01:49:46 +0100

    initial commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*{
+    Console.WriteLine("Hello");
+    Console.WriteLine("Welcome to the C#");
+}
+{
+    {
+        Console.WriteLine("First block");
+    }
+    {
+        Console.WriteLine("Second block");
+    }
+}
+string name = "Tom";
+Console.WriteLine(name);
+name = "Bob";
+Console.WriteLine(name);
+Console.WriteLine("Hello \n\nworld");
+// Tack 1 //
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+string Name = "Lera";
+int age = 24;
+double weight = 59.5;
+bool beautiful = true;
+Console.WriteLine($"Имя: {Name}, Возраст: {age}, Вес: {weight}, Кросивоя? {beautiful}");
+// Task 2 //
+string Name2 = "Lera";
+int Age = 24;
+double Weight = 59.5;
+Console.WriteLine("Имя: {0}  Возраст: {2}  Рост: {1}м", Name2, Weight, Age);
+// Task 3 //
+Console.Write($"Name: {name} Age: {age} Weight: {weight}");
+// Task 4 //
+Console.Write("\nType a name: ");
+string? name3 = Console.ReadLine();
+Console.WriteLine($"Hello {name3}-какашка!))))))))))))");
+Console.WriteLine("Tigr");
+Console.WriteLine('2');
+Console.WriteLine("Koshka");
+*/
+int x = 5;
+int b = --x;
+int c = x++;
+int z = c-- + ++x;
+Console.WriteLine(b + c + x + z);
+
