b5327bd5101e12971de1345d186b988280ae91f2
Valera-svg <215827524+Valera-svg@users.noreply.github.com>
2025-11-22 17:47:27 +0100

    A part of all

---
+++ domashka/23.11/23.11/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Вызов задач
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+        Task5();
+        Task6();
+    }
+    // =================== БЛОК 1. Условия ===================
+
+    // Задача 1
+    static void Task1()
+    {
+        int a = 10;
+        int b = 2;
+        if (a > b)
+            Console.WriteLine("True");
+        else
+            Console.WriteLine("False");
+    }
+    // Задача 2
+    static void Task2()
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(0, 101);
+        Console.WriteLine($"Generated number: {number}");
+
+        Console.Write("a=");
+        string aStr = Console.ReadLine();
+        Console.Write("b=");
+        string bStr = Console.ReadLine();
+
+        // преобразую строки в числа
+        int a = int.Parse(aStr);
+        int b = int.Parse(bStr);
+
+        if (a < number | b < number)
+            Console.WriteLine("False"); // a или b меньше сгенерированного числа: False
+       if (a > number & b > number)
+            Console.WriteLine("True"); // Оба числа больше сгенерированного: True
+    }
+    // Задача 3
+    static void Task3()
+    {
+        Console.WriteLine("Is it equal to 98?");
+        Console.Write("x=");
+        string xStr = Console.ReadLine();
+        Console.Write("y=");
+        string yStr = Console.ReadLine();
+
+        int x = int.Parse(xStr);
+        int y = int.Parse(yStr);
+
+        int z = 98;
+
+        if (x + y == z)
+            Console.WriteLine("True");
+        else if (x + y != z)
+            Console.WriteLine("False");
+    }
+    // Задача 4
+    static void Task4()
+    {
+        Console.Write("x=");
+        string xStr = Console.ReadLine();
+        Console.Write("y=");
+        string yStr = Console.ReadLine();
+        Console.Write("z=");
+        string zStr = Console.ReadLine();
+
+        int x = int.Parse(xStr);
+        int y = int.Parse(yStr);
+        int z = int.Parse(zStr);
+
+        if (x + y == z)
+            Console.WriteLine($"{x} + {y} = {z}");
+        else 
+            Console.WriteLine("False");
+    }
+    // Задача 5
+    static void Task5()
+    {
+        Console.Write("Type a string.");
+        string text = Console.ReadLine();
+
+        int count = text.Length;
+        int x = 13;
+
+        if (count > x)
+            Console.WriteLine("Error: Too much sharacters.");
+        else
+            Console.WriteLine(text);
+    }
+    // =================== БЛОК 2. Строки =================
+
+    // Задача 6
+    static void Task6()
+    {
+
+    }
+    // =================== БЛОК 3. Строки =================
+
+}
---
+++ domashka/HelloApp/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*{
+    Console.WriteLine("Hello");
+    Console.WriteLine("Welcome to the C#");
+}
+{
+    {
+        Console.WriteLine("First block");
+    }
+    {
+        Console.WriteLine("Second block");
+    }
+}
+string name = "Tom";
+Console.WriteLine(name);
+name = "Bob";
+Console.WriteLine(name);
+Console.WriteLine("Hello \n\nworld");
+
+// Tack 1 //
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+string Name = "Lera";
+int age = 24;
+double weight = 59.5;
+bool beautiful = true;
+Console.WriteLine($"Имя: {Name}, Возраст: {age}, Вес: {weight}, Кросивоя? {beautiful}");
+
+// Task 2 //
+string Name2 = "Lera";
+int Age = 24;
+double Weight = 59.5;
+Console.WriteLine("Имя: {0}  Возраст: {2}  Рост: {1}м", Name2, Weight, Age);
+
+// Task 3 //
+Console.Write($"Name: {name} Age: {age} Weight: {weight}");
+
+// Task 4 //
+Console.Write("\nType a name: ");
+string? name3 = Console.ReadLine();
+Console.WriteLine($"Hello {name3}-какашка!))))))))))))");
+Console.WriteLine("Tigr");
+Console.WriteLine('2');
+Console.WriteLine("Koshka");
+
+// Инкременты //
+int x = 5;
+int b = --x;
+int c = x++;
+int z = c-- + ++x;
+Console.WriteLine(b + c + x + z);
+
+// Домашка к 23.11 //
+Console.WriteLine("Hello! This solution is able to help you to count how many latters you have in your name. \nOf cours, you are free to use it to somehow else.");
+Console.Write("Give your name ");
+string name = Console.ReadLine();
+int SymbolCount = name.Length;
+Console.WriteLine($"Result is: {SymbolCount}");
+*/
+
+// block 1 условные операции //
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 2;
+        if (a > b)
+            Console.WriteLine("True");
+        else
+            Console.WriteLine("False");
+    }
+}
+
